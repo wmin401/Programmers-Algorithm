d25b364956678424d06f08fbb56c76aa8a3531b5
James Min <wmin401@gmail.com>
2023-11-20 00:13:40 +0900

    [level unrated] Title: 배열의 평균값, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    sum = 0
+    for i in numbers:
+        sum += i
+    
+    avg = sum / len(numbers)
+    # avg_list = avg.split(".")
+    # if avg_list[1] % 6 == 0
+    # answer = 0
+    return avg
