3f424e0651e78ab2dd286d92c633ef1ffdc1b762
James Min <wmin401@gmail.com>
2023-11-23 11:22:01 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -0,0 +1,11 @@
+def solution(dot):
+    answer = 0
+    if dot[0] >= 0 and dot[1] >= 0:
+        return 1
+    elif dot[0] < 0 and dot[1] >= 0:
+        return 2
+    elif dot[0] < 0 and dot[1] < 0:
+        return 3
+    elif dot[0] >= 0 and dot[1] < 0:
+        return 4
+   
