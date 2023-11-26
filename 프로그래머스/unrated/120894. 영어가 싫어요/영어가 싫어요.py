668457ac5355cdf472d439674806574fcd5b887c
James Min <wmin401@gmail.com>
2023-11-26 16:57:11 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    num_dic = {'zero': '0', 'one' : '1', 'two' : '2', 'three' : '3', 'four' : '4', 'five' : '5', 'six' : '6', 'seven' : '7', 'eight': '8', 'nine' : '9'}
+    
+    for key, value in num_dic.items():
+        numbers = numbers.replace(key, value)
+    
+    
+    return int(numbers)
