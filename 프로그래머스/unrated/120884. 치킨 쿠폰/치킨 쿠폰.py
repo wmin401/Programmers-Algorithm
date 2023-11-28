a5a9d464df9d6942609215b1f2bc165475a85878
James Min <wmin401@gmail.com>
2023-11-28 17:17:06 +0900

    [level unrated] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,12 @@
+def solution(chicken):
+    answer = 0
+
+    while chicken >= 10:
+        div, mod = divmod(chicken, 10)
+        
+        answer += div
+        chicken = div + mod
+        
+        
+        
+    return answer
