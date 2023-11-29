21e4068b5e3e571b18a99ad721aa8d85c8720a8a
James Min <wmin401@gmail.com>
2023-11-29 21:38:45 +0900

    [level unrated] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,8 @@
+def solution(common):
+    one, two, three = common[:3]
+    
+    if (two - one ) == (three - two):
+        result = common[-1] + (two - one)
+    elif (two // one) == (three // two):
+        result = common[-1] * (two // one)
+    return result
