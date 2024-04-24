6254218f08f094bac9026572ca89cd64440a79b0
James Min <wmin401@gmail.com>
2024-04-24 22:05:50 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.96 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -2,14 +2,18 @@ def solution(number, limit, power):
     answer = []
     
     for i in range(1, number + 1):
-        count = 0
-        for j in range(1, int(i**0.5 + 1)):
+        cnt = []
+        for j in range (1,  int(i**0.5) + 1):
             if i % j == 0:
-                count += 1
-                if j**2 != i:
-                    count += 1
-        if count <= limit:
-            answer.append(count)
+               cnt.append(j)
+               cnt.append(i//j)
+                    
+        answer.append(len(set(cnt)))
+    
+    total = 0
+    for i in answer:
+        if i > limit:
+            total +=  power
         else:
-            answer.append(power)
-    return sum(answer)
+            total += i
+    return total
