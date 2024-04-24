a0ee56ebc280296be95c65310bef2b69bb684739
James Min <wmin401@gmail.com>
2024-04-24 22:58:19 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,11 +1,18 @@
 def solution(N, stages):
-    people = len(stages)
-    faillist = {}
-    for i in range(1, N + 1):
-        if people != 0:
-            faillist[i] = stages.count(i) / people
-            people -= stages.count(i)
-        else:
-            faillist[i] = 0
     
-    return sorted(faillist, key = lambda i: faillist[i], reverse=True)
+    failRate = {}
+    people = len(stages)
+    
+    for i in range(1, N+1):
+        if people !=0:
+            failRate[i] = stages.count(i)/people
+            people = people - stages.count(i)
+        else:
+            failRate[i] = 0
+        
+    
+    return sorted(failRate, key=lambda x: failRate[x], reverse=True)
+        
+        
+    
+    
