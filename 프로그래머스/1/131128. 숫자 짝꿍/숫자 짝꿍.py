d8096646d9037c0e46592259078be4d1e3cccd9e
James Min <wmin401@gmail.com>
2024-04-25 18:46:58 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,12 +1,12 @@
 def solution(X, Y):
-    answer = ''
+    answer =  ''
     
-    for i in range(9, -1, -1):
-        answer += (str(i) * min(X.count(str(i)), Y.count(str(i))))
+    for i in range(9,-1,-1):
+        answer += str(i) * min(X.count(str(i)), Y.count(str(i)))
         
     if answer == '':
-        return '-1'
+        return "-1"
     elif len(answer) == answer.count('0'):
-        return '0'
+        return "0"
     else:
         return answer
