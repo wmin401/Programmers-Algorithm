27662d6b77b989a18b72c82c515d9995b171c7fa
James Min <wmin401@gmail.com>
2024-04-26 18:00:55 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.87 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,6 +1,6 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256?language=python3) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 26일 18:00:39
+2024년 04월 26일 18:00:53
 
 ### 문제 설명
 
--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,39 +1,13 @@
 def solution(numbers, hand):
-    
-    number_dic = {1:(0,0), 2:(0,1), 3:(0,2),
-                  4:(1,0), 5:(1,1), 6:(1,2),
-                  7:(2,0), 8:(2,1), 9:(2,2),
-                  "*":(3,0), 0:(3,1), "#":(3,2)}
-    
-    lhand = number_dic["*"]
-    rhand = number_dic["#"]
-    
-    
     answer = ''
     
     for i in numbers:
         if i in [1,4,7]:
             answer += 'L'
-            lhand = number_dic[i]
-            
         elif i in [3,6,9]:
             answer += 'R'
-            rhand = number_dic[i]
-            
-        elif i in [2,5,8,0]:
-            if abs(number_dic[i][0] - lhand[0]) + abs(number_dic[i][1] - lhand[1]) < abs(number_dic[i][0] - rhand[0]) + abs(number_dic[i][1] - rhand[1]):
-                answer += 'L'
-                lhand = number_dic[i]
-            elif abs(number_dic[i][0] - lhand[0]) + abs(number_dic[i][1] - lhand[1]) > abs(number_dic[i][0] - rhand[0]) + abs(number_dic[i][1] - rhand[1]):
-                answer += 'R'
-                rhand = number_dic[i]
-            
-            else: 
-                if hand == 'right':
-                    answer += 'R'
-                    rhand = number_dic[i]
-                else:
-                    answer += 'L'
-                    lhand = number_dic[i]
-                    
+        elif hand == 'left' and i in [2,5,8,0]:
+            answer += 'L'
+        elif hand == 'right' and i in [2,5,8,0]:
+            answer += 'R'
     return answer
